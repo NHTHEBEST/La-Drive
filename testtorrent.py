69c8bb1dbf1cb9a26a676972349fcb8777a17a1a
cosmo.zavoda <cosmo.zavoda@icoud.com>
2019-09-26 15:24:51 +1200

    yeet

--- testtorrent.py
+++ testtorrent.py
@@ -20,10 +20,10 @@ while (not handle.has_metadata()):
 	time.sleep(1)
 print 'got metadata, starting torrent download...'
 while (handle.status().state != lt.torrent_status.seeding):
-	 s = handle.status()
-	 state_str = ['queued', 'checking', 'downloading metadata', \
-				 'downloading', 'finished', 'seeding', 'allocating']
-	 print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %.3' % \
+	s = handle.status()
+	state_str = ['queued', 'checking', 'downloading metadata', \
+				'downloading', 'finished', 'seeding', 'allocating']
+	print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %.3' % \
 			(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
 			s.num_peers, state_str[s.state], s.total_download/1000000)
-	 time.sleep(5)
+	time.sleep(5)
