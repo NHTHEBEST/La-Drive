488fe5e877ceab776ffd28ebf911342b571a3295
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2019-10-25 06:44:26 +1300

    Update DownUpHash.py

--- DownUpHash.py
+++ DownUpHash.py
@@ -19,21 +19,25 @@
 
 #-------------------------------------------------------------------------------
 # globals
-<<<<<<< HEAD
-=======
+#<<<<<<< HEAD
+#=======
 URL     = "http://noserver.hadar.net.nz/thedrive/"       #URL for downloading magnet link
->>>>>>> dbce5bc6b8bc41261eb33f610150b20a88fbaaee
-global URL
+#>>>>>>> dbce5bc6b8bc41261eb33f610150b20a88fbaaee
+#global URL
 DELFILE = True    #Deletes downloaded data after uploading
-global DELFILE
-SAVEPATH= "./stuff/"       #Path to dumped data 
-global SAVEPATH
+#global DELFILE
+SAVEPATH= "/tmp/stuff/"       #Path to dumped data 
+#global SAVEPATH
 UPDDELAY= 60       #Delay between database syncs
-global UPDDELAY
+#global UPDDELAY
 UPLOADCMD = "gdrive upload -p 1BMRqZT7fwv0X_ZzweNrePfd3pgqaJ8Il -r "
-global UPLOADCMD
+#global UPLOADCMD
 HASH    = ""       #hash to download
-global HASH
+#global HASH
+#Trakers
+TRAKERS_file = open("trakers.txt","r")
+TRAKERS = TRAKERS_file.read()
+#print(TRAKERS)
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
@@ -66,44 +70,48 @@ def torrent():
 		'paused': False,
 		'auto_managed': True,
 		'duplicate_is_error': True}
-	link = "magnet:?xt=urn:btih:"HASH
+	link = "magnet:?xt=urn:btih:"+HASH+TRAKERS
+	print(link)
 	handle = lt.add_magnet_uri(ses, link, params)
 	ses.start_dht()
 
-	print 'downloading metadata...'
+	print('downloading metadata...')
 	while (not handle.has_metadata()):
 		time.sleep(1)
-	print 'got metadata, starting torrent download...'
+		print("#", end = '')
+	print('got metadata, starting torrent download...')
 	while (handle.status().state != lt.torrent_status.seeding):
 		 s = handle.status()
 		 state_str = ['queued', 'checking', 'downloading metadata', \
 					 'downloading', 'finished', 'seeding', 'allocating']
-		 print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %.3' % \
-				(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
-				s.num_peers, state_str[s.state], s.total_download/1000000)
+		# print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %.3' % \
+		#		(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+		#		s.num_peers, state_str[s.state], s.total_download/1000000)
+		 print(s.progress*100,end="\r")
 		 time.sleep(5)
 #-------------------------------------------------------------------------------
 #get the hash from server
 def gethash():
 	output = requests.get(URL+"get.php").text
-	print("hash is:"output)
+	print("hash is:"+output)
 	print("url reveived")
 	return output
 #-------------------------------------------------------------------------------
 #upload file to the drive
 def upload():
-	os.system(UPLOADCMD+str(SAVEPATH))
+	os.system("rm /tmp/stuff/**/*.jpg /tmp/stuff/**/*.txt /tmp/stuff/**/*.sub /tmp/stuff/**/*.png /tmp/stuff/**/*.str")
+	os.system(UPLOADCMD+str(SAVEPATH)+"*")
 	print("file uploaded")
 #-------------------------------------------------------------------------------
 #delete uploaded data
 def deldata():
 	if DELFILE:
-		os.system("rm -r "SAVEPATH"*")
+		os.system("rm -r "+SAVEPATH+"*")
 		print("File deleted")
 #-------------------------------------------------------------------------------
 # updates the server
 def updateserver(hash):
-	requests.get(URL+"/rm.php?key=Thekey&hash="+hash)
+	requests.get(URL+"/rm.php?key=625c93b5-fbfe-4ea5-921d-27b7da51aa10&hash="+hash)
 	print("server updated")
 #-------------------------------------------------------------------------------
 #main
